b449b1d62e98a7912844ee796fb34ab1eda4ce0a
Katharina Braasch <katharina.braasch@stud.uni-due.de>
2023-05-14 23:06:41 +0200

    Falle stellen und troubleshooting

--- Computer_Player.py
+++ Computer_Player.py
@@ -73,6 +73,51 @@ def make_combo_move(n, p):
                     return True
     return False
 
+def setup_trap():
+
+    print("trying to setup trap")
+    # 2 GK die frei sind bis auf 1Feld(was mit 1 belegt ist(Reacy))  und  1 gemeinsames Feld haben
+    #gesuchte GK: SUM==1 (combovalue)
+
+    # Problem: -1 + 1 + 1 == 1
+    # board[]: [[x,x,x],
+    #           [x,x,x],
+    #           [x,x,x]]
+
+    einDboard = []
+
+    #Transformation des Boards (eindimensional mit 4 statt -1)
+    for k in range(3):
+        for j in range(3):
+            if board[k][j] == -1:
+                einDboard.append(4)
+            else:
+                einDboard.append(board[k][j])
+
+        # einDboard = [x,x,x,x,x,x,x,x]
+        #Problem solved
+
+    GKv1 = []
+
+    #Finden der GK mit Summe 1 (Belegung: 0 + 0 + 1) (Gkv1)
+    for combo in range(len(wincombinations)):
+        if combovalue(combo) == 1:
+            GKv1.append(wincombinations[combo])
+
+    print(GKv1)
+        #GKv1 = [ [[x,x],[x,x],[x,x]], [[x,x],[x,x],[x,x]], [[x,x],[x,x],[x,x]] ]
+
+    if len(GKv1) > 1: #and reachy_moveCounter > 1
+        #Gemeinsames Feld zweier GKv1 finden
+        for k in range(len(GKv1) - 1):
+            for j in range(3):
+                for i in range(k+1, len(GKv1)):
+                    for h in range(3):
+                        if GKv1[k][j] == GKv1[i][h] and board[GKv1[k][j][0]][GKv1[k][j][1]] == 0:
+                            print("k=",k, "j=",j)
+                            board[k][j] == 1
+                            return True
+    return False            
 
 def corner_move():
     print("trying to make corner_move")
@@ -81,7 +126,7 @@ def corner_move():
         if board[corners[k][0]][corners[k][1]] == 0:
             free_corner = True
     while free_corner:
-        i = random.randint(0, 4)
+        i = random.randint(0, 3)
         if board[corners[i][0]][corners[i][1]] == 0:
             board[corners[i][0]][corners[i][1]] = 1
             return True
@@ -96,6 +141,7 @@ def make_good_move(p):
 
     if reachy_moveCounter == 0 and player_moveCounter == 1:
         # try the middle cell and make move if possible
+        print("Zug 1.1")
         if board[1][1] == 0:
             board[1][1] = 1
             return True
@@ -104,48 +150,23 @@ def make_good_move(p):
                 return True
 
     elif reachy_moveCounter == 1 and player_moveCounter == 1:
+        print("Zug 2.0")
         if corner_move():
             return True
 
     elif reachy_moveCounter == 1 and player_moveCounter == 2:
         # FALLE VERHINDERN
-        print("HIER!!")
-        if combovalue(board, 6) == -1 or combovalue(board, 7) == -1:
+        print("Zug 2.1")
+        if combovalue(6) == -1 or combovalue(7) == -1:
             x = 1
             y = random.randint(0, 2)
             randfeld = [x, y]
             a = random.sample(randfeld, 2)
-            board[a[0]][a[1]] = 1
+            print(a)
+            board[a[0]][a[1]] = 1 ##PROBELM HIER
             return True
     # TODO: Zug == 2.1: bei 4+1+4:Rand Feld, sonst Gewinnkombination mit Summe = 1 = 1+0+0
-    # if board[1][1] == 0:
-    #     board[1][1] = 1
-    #     check_state()
-    #     return True
-    # # else try to get into a corner cell
-    # else:
-    #     if board[0][0] == 0 or board[0][2] == 0 or board[2][0] == 0 or board[2][2] == 0:
-    #         print("picking a corner")
-    #         while 1 != 0:
-    #             x = random.randint(0, 3)
-    #             if x == 0 and board[0][0] == 0:
-    #                 board[0][0] = 1
-    #                 check_state()
-    #                 return True
-    #             elif x == 1 and board[0][2] == 0:
-    #                 board[0][2] = 1
-    #                 check_state()
-    #                 return True
-    #             elif x == 2 and board[2][0] == 0:
-    #                 board[2][0] = 1
-    #                 check_state()
-    #                 return True
-    #             elif x == 3 and board[2][2] == 0:
-    #                 board[2][2] = 1
-    #                 check_state()
-    #                 return True
-    #     else:
-    #         return False
+
     return False
 
 
@@ -187,7 +208,8 @@ def make_computer_move(currentboard, currentlevel, reachy_moves, player_moves):
     p = random.randint(0, 100)
     if not make_combo_move(2, p):
         if not make_combo_move(-2, p):
-            if not make_good_move(p):
-                make_random_move()
+            if not setup_trap():
+                if not make_good_move(p):
+                    make_random_move()
     reachy_moveCounter = reachy_moveCounter + 1
     return board
