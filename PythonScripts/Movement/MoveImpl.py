419ef9628ebfc1aa097757449d7e0825152e2c0b
michalitv21 <130777255+michalitv21@users.noreply.github.com>
2023-05-23 10:21:13 +0200

    basePosition as list

--- PythonScripts/Movement/MoveImpl.py
+++ PythonScripts/Movement/MoveImpl.py
@@ -23,7 +23,7 @@ class MoveImpl:
         self._move_arm(constants.POS_BASE_COORDINATES)
         # Defines Dictionary for modifying the gripping force
         self.POS_GRIPPER = {self.reachy.r_arm.r_gripper: 0}
-        self.basePosition = (0.36, -0.20, 0)
+        self.basePosition = [0.36, -0.20, 0]
 
     def getBasePos(self):
         return self.basePosition
