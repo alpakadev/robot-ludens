eab077467951bf7e9c9fe63753d886059f7d51cf
Louis Born <louis.born@check24.de>
2023-05-30 15:31:23 +0200

    removed unnecessary pass statement

--- PythonScripts/Movement/MoveImpl.py
+++ PythonScripts/Movement/MoveImpl.py
@@ -10,7 +10,8 @@ from .Enums.Board import Board
 from .Enums.Outside import Outside
 from .Helper.HandRotationMapper import HandRotationMapper
 
-#get the current arm position (Matrix): reachy_sdk.r_arm.forward_kinematics()
+
+# get the current arm position (Matrix): reachy_sdk.r_arm.forward_kinematics()
 
 # Docs:
 # https://docs.pollen-robotics.com/sdk/first-moves/kinematics/#forward-kinematics
@@ -18,14 +19,12 @@ from .Helper.HandRotationMapper import HandRotationMapper
 # https://pollen-robotics.github.io/reachy-2019-docs/docs/program-your-robot/control-the-arm/#forward-kinematics
 
 class MoveImpl:
-    
-    
 
     def __init__(self):
         self.kinematic_model_helper = KinematicModelHelper()
         # Its the origin/reference point, to which all other coordinates of the Board and the Blocks are relative.
-        self.origin_coordinate = [0.1, -0.296, -0.37] # Previous name: basePosition
-    
+        self.origin_coordinate = [0.1, -0.296, -0.37]  # Previous name: basePosition
+
     def set_dependencies(self, reachy: ReachySDK, perc, strat):
         self.reachy = reachy
         self.perc = perc
@@ -35,7 +34,7 @@ class MoveImpl:
 
     def set_arm_to_right_angle_position(self):
         self._move_arm(constants.POS_ARM_AT_RIGHT_ANGLE, rotation={'y': -90, 'x': 0, 'z': 0})
-    
+
     def set_arm_to_origin(self):
         self._move_arm(self.origin_coordinate, rotation={'y': -90, 'x': 0, 'z': 0})
 
@@ -46,13 +45,13 @@ class MoveImpl:
     def set_origin(self, coordinate):
         # Returns the origin point to which all other coordinates are relative from
         self.origin_coordinate = coordinate
-        
-    def addlists(self,a,b):
+
+    def addlists(self, a, b):
         c = a[::]
         for i in range(len(c)):
             c[i] += b[i]
         return c
-    
+
     def activate_right_arm(self):
         self.reachy.turn_on("r_arm")
 
@@ -68,12 +67,11 @@ class MoveImpl:
         """
         self.activate_right_arm()
         self.set_arm_to_right_angle_position()
-        
+
         mapper = HandRotationMapper()
         # Setting arm joints to Stiff-mode for starting movement
-        
+
         # Setting head joints to stiff mode
-        
 
         pos_to_value = pos_to_enum.value
         pos_from_value = pos_from_enum.value
@@ -81,7 +79,7 @@ class MoveImpl:
         # Adds the position values to base position - Since the Enums are dependent of the Base Position
         pos_from_value = self.addlists(self.origin_coordinate, pos_from_value)
         pos_to_value = self.addlists(self.origin_coordinate, pos_to_value)
-        
+
         # Tiefe == x (nach vorne), breite == z , Hoehe ==y
         pos_from_value[1] += constants.DELTA_HAND_WIDTH  # Non Moving Part of Hand would knock Items over
         # starting movement of reachy's head
@@ -97,7 +95,7 @@ class MoveImpl:
         # 2. Opens Hand
         self._grip_open()
         # 3. Moves Hand/arm to the object
-        #pos_from_value[0] += constants.DELTA_HAND_TIP # or else its just the Tip around the cylinder
+        # pos_from_value[0] += constants.DELTA_HAND_TIP # or else its just the Tip around the cylinder
         self._move_arm(pos_from_value, rotation={'y': -90, 'x': 0, 'z': 0})
         # 4. closes Hand
         self._grip_close()
@@ -105,10 +103,11 @@ class MoveImpl:
         pos_from_value[2] += constants.DELTA_ABOVE_OBJ
         self._move_arm(pos_from_value, rotation={'y': -90, 'x': 0, 'z': 0})
         pos_from_value[2] -= constants.DELTA_ABOVE_OBJ
-        #self.move_head(pos_goal)
+        # self.move_head(pos_goal)
         # 6. Moves arm above pos_to
         pos_to_value[2] += constants.DELTA_ABOVE_OBJ
-        self._move_arm(pos_to_value, rotation={'y': -90, 'x': 0, 'z': mapper.get_hand_rotation(pos_to_enum)})  ##TODO: How to Handle POS_ABOVE_BOARD?
+        self._move_arm(pos_to_value, rotation={'y': -90, 'x': 0, 'z': mapper.get_hand_rotation(
+            pos_to_enum)})  ##TODO: How to Handle POS_ABOVE_BOARD?
         pos_to_value[2] -= constants.DELTA_ABOVE_OBJ
         self.move_head()
         # 7. moves arm to pos_to
@@ -130,7 +129,7 @@ class MoveImpl:
         self.deactivate_right_arm()
         # head back to default and setting head to compliant mode
         self.move_head(constants.HEAD_LOOK_AHEAD)
-        
+
         pass
 
     def _move_arm(self, pos_to: list, rotation: dict):
@@ -148,8 +147,7 @@ class MoveImpl:
 
     def _change_grip_force(self, force):
         self.POS_GRIPPER[self.reachy.r_arm.r_gripper] = force
-        #print("current force:", self.POS_GRIPPER[self.reachy.r_arm.r_gripper])
-        pass
+        # print("current force:", self.POS_GRIPPER[self.reachy.r_arm.r_gripper])
 
     def _grip_open(self):
         """
@@ -158,7 +156,6 @@ class MoveImpl:
         OPEN_FULL = -60
         self._change_grip_force(OPEN_FULL)
         goto(goal_positions=self.POS_GRIPPER, duration=1.0, interpolation_mode=InterpolationMode.MINIMUM_JERK)
-        pass
 
     def _grip_close(self):
         """
@@ -174,7 +171,6 @@ class MoveImpl:
             self._change_grip_force(force)
             force += 1
         goto(goal_positions=self.POS_GRIPPER, duration=1.0, interpolation_mode=InterpolationMode.MINIMUM_JERK)
-        pass
 
     def _is_holding(self):
         """
@@ -192,7 +188,7 @@ class MoveImpl:
 
         :param pos_to (array): Coordinates on where to move the object
         """
-        self.reachy.mobile_base.goto(x,y, theta=0)
+        self.reachy.mobile_base.goto(x, y, theta=0)
 
     def turn_body(self, degree):
         """
@@ -203,15 +199,15 @@ class MoveImpl:
 
     def calibrate(self):
         matrix = self.reachy.r_arm.forward_kinematics()
-        x = round(matrix[0][3],2)
-        y = round(matrix[1][3],2)
+        x = round(matrix[0][3], 2)
+        y = round(matrix[1][3], 2)
         z = -0.37
-        res = [x,y,z]
+        res = [x, y, z]
         self.set_origin(res)
         print(self.get_origin())
-        #print(matrix)
-        #print(matrix[0][3])
-    
+        # print(matrix)
+        # print(matrix[0][3])
+
     def get_position(self):
         """
         Returns current Cartesian coordinitas Position
@@ -221,23 +217,23 @@ class MoveImpl:
         """
         pass
 
-    def move_head(self, look_at = None):
+    def move_head(self, look_at=None):
         """
         Moves reachy's head either by the given coordinates defined by look_at or
         follows the right arm's coordinates
 
         """
-        #turn on head
+        # turn on head
         self.reachy.turn_on("head")
 
         # head follows arm
         if look_at is None:
-            x, y, z = self.reachy.r_arm.forward_kinematics()[:3,-1]
-            self.reachy.head.look_at(x=x, y=y, z=z-0.05, duration=1.0)
+            x, y, z = self.reachy.r_arm.forward_kinematics()[:3, -1]
+            self.reachy.head.look_at(x=x, y=y, z=z - 0.05, duration=1.0)
 
         # head looks at given x,y,z
         else:
-            x,y,z = look_at
+            x, y, z = look_at
             self.reachy.head.look_at(x=x, y=y, z=z, duration=1.0)
 
         self.reachy.turn_off_smoothly("head")
@@ -245,27 +241,27 @@ class MoveImpl:
 
 if __name__ == "__main__":
     # Instantiate reachy instance
-    #reachy_sdk = ReachySDK(host=constants.HOSTADDRESS, with_mobile_base=True) # Mobile base problems with Simulations
+    # reachy_sdk = ReachySDK(host=constants.HOSTADDRESS, with_mobile_base=True) # Mobile base problems with Simulations
     reachy_sdk = ReachySDK(host=constants.HOSTADDRESS)
 
     robot = MoveImpl()
     robot.set_dependencies(reachy_sdk, None, None)
-    
-    #robot.move_object(Outside.BLOCK_1, Board.TOP_LEFT)
-    #robot.move_object(Outside.BLOCK_2, Board.CENTER_LEFT)
-    #robot.move_object(Outside.BLOCK_3, Board.BOTTOM_LEFT)
-    #robot.move_object(Outside.BLOCK_4, Board.CENTER)
-    #robot.move_object(Outside.BLOCK_5, Board.TOP_RIGHT)
-    
-    #reachy_sdk.turn_on("reachy")
+
+    # robot.move_object(Outside.BLOCK_1, Board.TOP_LEFT)
+    # robot.move_object(Outside.BLOCK_2, Board.CENTER_LEFT)
+    # robot.move_object(Outside.BLOCK_3, Board.BOTTOM_LEFT)
+    # robot.move_object(Outside.BLOCK_4, Board.CENTER)
+    # robot.move_object(Outside.BLOCK_5, Board.TOP_RIGHT)
+
+    # reachy_sdk.turn_on("reachy")
 
     # [depth, width, height]
     # Unity: depth(front) == -x , width(side) == -z , height() == y
-    #robot.arm_to_init_pos()
-    #robot.move_object(Outside.BLOCK_1, Board.TOP_RIGHT)
-    #robot.move_object(Outside.BLOCK_2, Board.CENTER)
-    
-    #import time
-    #time.sleep(5)
-    #reachy_sdk.turn_off_smoothly("reachy")
-    #robot.move_object(Outside.BLOCK_1, Board.TOP_RIGHT)
+    # robot.arm_to_init_pos()
+    # robot.move_object(Outside.BLOCK_1, Board.TOP_RIGHT)
+    # robot.move_object(Outside.BLOCK_2, Board.CENTER)
+
+    # import time
+    # time.sleep(5)
+    # reachy_sdk.turn_off_smoothly("reachy")
+    # robot.move_object(Outside.BLOCK_1, Board.TOP_RIGHT)
