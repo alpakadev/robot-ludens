74c7179375e341b845cf3d98c4694a6dec6ebe91
KatjanNA <131463322+KatjanNA@users.noreply.github.com>
2023-05-30 23:40:31 +0200

    Update Loose.py

--- PythonScripts/Movement/Animations/Loose.py
+++ PythonScripts/Movement/Animations/Loose.py
@@ -1,7 +1,5 @@
 import time
 
-from .SadAntennas import animation_sad_antennas
-
 
 def animation_loose(reachy):
     reachy.head.look_at(0.05, 0, 0, duration=0.5)
@@ -10,17 +8,14 @@ def animation_loose(reachy):
 
     reachy.head.look_at(0.5, 0, -0.4, 0.5)
 
-    time.sleep(1.5)
+    time.sleep(0.5)
 
-    reachy.head.l_antenna.speed_limit = 150.0
-    reachy.head.r_antenna.speed_limit = 150.0
+    reachy.head.l_antenna.speed_limit = 90.0
+    reachy.head.r_antenna.speed_limit = 90.0
     reachy.head.l_antenna.goal_position = 140.0
     reachy.head.r_antenna.goal_position = -140.0
 
-    time.sleep(2.0)
+    time.sleep(1.0)
 
-    reachy.head.look_at(0.05, -0.02, -0.04, duration=0.5)
-
-    animation_sad_antennas(reachy)
-
+    reachy.head.look_at(0.05, -0.03, -0.04, duration=0.5)
     reachy.head.look_at(0.5, 0, -0.0, 1.0)
