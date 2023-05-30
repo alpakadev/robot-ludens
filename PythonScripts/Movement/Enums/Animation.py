5a0198e240b754cc3058d66912ff75503ad0cd4b
KatjanNA <131463322+KatjanNA@users.noreply.github.com>
2023-05-31 00:55:28 +0200

    update Animation.py

--- PythonScripts/Movement/Enums/Animation.py
+++ PythonScripts/Movement/Enums/Animation.py
@@ -4,6 +4,9 @@ from enum import Enum
 class Animation(Enum):
     WIN = 'win',
     LOOSE = 'loose',
+    ANGRY = 'angry',
+    DISAPPROVAL = 'disapproval'
+    THINKING = "thinking",
 
     """
     Could add more in here... e.g.:
