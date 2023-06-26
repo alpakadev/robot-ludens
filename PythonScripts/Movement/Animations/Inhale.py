93e388207e80926a3419e36683c097c592d9b575
alpakadev <64690504+alpakadev@users.noreply.github.com>
2023-06-26 13:51:42 +0200

    relative Path to Soundclip

--- PythonScripts/Movement/Animations/Inhale.py
+++ PythonScripts/Movement/Animations/Inhale.py
@@ -1,8 +1,11 @@
-from.Player import play_sound 
+from.Player import play_sound # pip install playsound
 
-inhale =  "PythonScripts/Movement/Sounds/inhaling.mp3" 
+# TODO Should not be a Global variable
+inhale =  "./PythonScripts/Movement/Sounds/inhaling.mp3"
 
-#sound isnt played through reachy's speakers...fix needed
+# sound isnt played through reachy's speakers...~fix needed~
+# fix: Project needs to be run on Reachy.
+# Playsound does not send the Soundfiles to reachy
 def animation_inhale():
     
     play_sound(inhale, block = True) 
