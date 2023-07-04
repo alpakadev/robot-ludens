a4e0028eaf97747220cbc5b22148dccee57bb6b7
smphfres <philip.frese@stud.uni-due.de/bin/python>
2023-07-04 12:51:12 +0200

    Unnötige imports entfernt und implementierung vervollständigt

--- PythonScripts/Perception/FaceRecognition/recognition.py
+++ PythonScripts/Perception/FaceRecognition/recognition.py
@@ -1,13 +1,14 @@
 import dlib
 import face_recognition as fr
 import cv2
-from PIL import Image
 import numpy as np
 import time
 from math import ceil, radians, sin
+import json
+from Perception.FaceRecognition.NumpyEncoder import NumpyEncoder
 
 # Durchsucht ein aufgenommenes Bild auf Gesichter und entnimmt das größte
-def detect_human_player(reachy):
+def identify_human_player(reachy, move):
     # Reachys Kopf in Basis Position bringen
     reachy.turn_on("head")
     reachy.head.look_at(1, 0, 0, 1, "simul")
@@ -20,7 +21,7 @@ def detect_human_player(reachy):
     face_encoding = []
     face_box = []
     for face in face_locations:
-        enc = fr.face_encodings(image, known_face_locations=[face], num_jitters=10, model="large")
+        enc = fr.face_encodings(image, known_face_locations=[face], num_jitters=5, model="large")
         size = (face[2] - face[0]) * (face[1] - face[3])
         if size > box_size:
             box_size = size
@@ -29,21 +30,33 @@ def detect_human_player(reachy):
 
     # Größtes Gesicht ermitteln
     cv2.rectangle(image, (face_box[3], face_box[0]), (face_box[1], face_box[2]), (255, 0, 0), 4)
-    cv2.imwrite("Player.png", image)
+    cv2.imwrite("PythonScripts/Perception/FaceRecognition/Player.png", image)
+    
     print("Face Detected")
     return face_encoding
     
+def serialize_player_face(face):
+    with open("PythonScripts/Perception/FaceRecognition/face_encoding.json", "w", encoding="utf-8") as file:
+        json.dump({"playerFace": face}, file, cls=NumpyEncoder, ensure_ascii=False, indent=4)
+
+def deserialize_player_face():
+    with open("PythonScripts/Perception/FaceRecognition/face_encoding.json", "r", encoding="utf-8") as file:
+        encoding = json.load(file)
+    return np.array(encoding["playerFace"])
+
 # Steuer Funktion um Reachy dazu zubringen den Player anzuschauen
-def look_at_human_player(reachy, face_enc):
+def look_at_human_player(reachy, move, face_enc):
     print("Looking at Player")
     image = take_picture(reachy)
     player_face_pos = compare_faces_for_pos(image, face_enc)
+    print(player_face_pos)
 
     if player_face_pos is not None:
-        center_vision_on_face(image, player_face_pos, face_enc, reachy)
+        center_vision_on_face(reachy, move, image, player_face_pos, face_enc)
     else:
         print("Not in View")
-        reachy.head.look_at(0.5, 0, 0, 1, "simul")
+        #move.do_move_head([0.5, 0, 0, 1])
+        reachy.head.look_at(1, 0, 0, 1, "simul")
 
 # überprüft ob sich das Player Gesicht im Bild befindet und wenn ja, dann wo
 def compare_faces_for_pos(image, player_face_enc):
@@ -52,7 +65,9 @@ def compare_faces_for_pos(image, player_face_enc):
     face_box = []
 
     for face in face_positions:
-        unknown_face_enc = fr.face_encodings(image, known_face_locations=[face], num_jitters=10, model="large")
+        unknown_face_enc = fr.face_encodings(image, known_face_locations=[face], num_jitters=5, model="large")
+        print("player", player_face_enc[0], "unknonw", unknown_face_enc[0])
+
         results = fr.face_distance([unknown_face_enc[0]], player_face_enc[0])
         if smallest_distance is None:
             smallest_distance = results
@@ -61,7 +76,6 @@ def compare_faces_for_pos(image, player_face_enc):
             smallest_distance = results
             face_box = face
     
-    print(smallest_distance, face_box)
     return face_box
 
 def take_picture(reachy):
@@ -82,16 +96,13 @@ def get_largest_face(face_locations):
     return largest_face
 
 # Berechnet wie sich der Kopf bewegen muss um den Player direkt anzuschauen
-def center_vision_on_face(image, face_pos, player_face_enc, reachy):
+def center_vision_on_face(reachy, move, image, face_pos, player_face_enc):
     print("Moving head to look at Player")
     # Dimensionen das aufgenommenen Bildes speichern
-    im_width, im_height, _ = image.shape
-    print("w", im_width, "h", im_height)
+    im_height, im_width, _ = image.shape
 
     bottom, left, top, right = face_pos
     face_center = [ceil(right + (left - right) / 2), ceil(bottom + (top - bottom) / 2)]
-    cv2.circle(image, (face_center[0], face_center[1]), 5, (255, 0, 0), 3)
-    cv2.imwrite("center.png", image)
     # Abweichung der Oberen rechten Ecke von den Mittellinien des Bildes ausrechnen
     x_diff = face_center[0] - im_width/2
     y_diff = face_center[1] - im_height/2
@@ -104,9 +115,18 @@ def center_vision_on_face(image, face_pos, player_face_enc, reachy):
     x_coor = ceil(im_width / 2 + im_width * (x_perc_contr * 0.5))
     y_coor = ceil(im_height / 2 + im_height * (y_perc_contr * 0.05))
 
-    cv2.circle(image, (x_coor, y_coor), 5, (0, 255, 0), 3)
-    cv2.imwrite("control.png", image)
-    reachy.head.look_at(1, -1 * x_perc_contr, -1 * y_perc_contr, 1, "simul")
+    distance_to_move_horizontally = x_perc_contr * -0.5
+    distance_to_move_vertically = y_perc_contr * -0.5
+
+    if distance_to_move_horizontally < 0:
+        distance_to_move_horizontally += 0.05
+    else:
+        distance_to_move_horizontally -= 0.05
+
+    #move.do_move_head([1, distance_to_move_horizontally, distance_to_move_vertically, 1])
+    reachy.head.look_at(1, distance_to_move_horizontally, distance_to_move_vertically, 1, "simul")
+    reachy.head.turn_off_smoothly()
+
 
 def calc_center_diff(p_c_diff, im_dim):
     lense_radius = 10
