d1c1c6f1cbcd38efb0b4ad9ac27bc416195527ec
Raphael Stedler <r.stedler@gmx.de>
2023-07-07 15:23:39 +0200

    removed get_unused_pieces_from_frame

--- PythonScripts/Perception/PiecePerception/PiecePerception.py
+++ PythonScripts/Perception/PiecePerception/PiecePerception.py
@@ -1,4 +1,3 @@
-from .unused_pieces_detection import unused_pieces_detection
 from .nearest_unused_piece import get_nearest_unused_piece
 from .game_figure_detection import get_all_pieces_coordinates
 from .estimate_metric_distance import estimate_metric_distance
@@ -6,9 +5,6 @@ from .estimate_metric_distance import estimate_metric_distance
 class PiecePerception:
     def __init__(self, config):
         self.config = config
-    
-    def get_unused_pieces_from_frame(self, frame):
-        return unused_pieces_detection(frame)
     
     def get_nearest_unused_piece(self, frame, board_corners):
         return get_nearest_unused_piece(frame, board_corners)
