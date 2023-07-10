56aa5445030c376d931e1c2a0ef5c372df47504c
Raphael Stedler <r.stedler@gmx.de>
2023-07-10 16:31:57 +0200

    adapted get_all_pieces_coordinates and estimate_metric_distance

--- PythonScripts/Perception/PiecePerception/game_figure_detection.py
+++ PythonScripts/Perception/PiecePerception/game_figure_detection.py
@@ -8,17 +8,18 @@ def get_all_pieces_coordinates(frame, board_coordinates,
     imageFrame = frame.copy()
     # config = yaml.safe_load(open("global_config.yml"))
 
-    piece_centers = [0] * 9
-
     # Define the lower and upper bounds for red and green color
-    lower_green = np.array([35, 50, 50])
-    upper_green = np.array([90, 255, 255])
+    # lower_green = np.array([35, 50, 50])
+    # upper_green = np.array([90, 255, 255])
 
-    lower_red = np.array([0, 70, 50])
-    upper_red = np.array([10, 255, 255])
+    lower_green = np.array([40, 50, 50], dtype=np.uint8)
+    upper_green = np.array([80, 255, 255], dtype=np.uint8)
+
+    lower_red = np.array([0, 70, 50], dtype=np.uint8)
+    upper_red = np.array([10, 255, 255], dtype=np.uint8)
 
     # Crop the image to get only the shape of the board
-    mask = np.zeros(frame.shape[:2], dtype="uint8")
+    mask = np.zeros(imageFrame.shape[:2], dtype="uint8")
     roi = np.array(board_coordinates)
     cv2.fillPoly(mask, [roi], (255, 255, 255))
     masked = cv2.bitwise_and(imageFrame, imageFrame, mask=mask)
@@ -26,17 +27,20 @@ def get_all_pieces_coordinates(frame, board_coordinates,
     # Convert the image to HSV color space
     hsv = cv2.cvtColor(masked, cv2.COLOR_BGR2HSV)
 
-    for case_coords in board_cases_coordinates:
-        case = board_cases_coordinates[case_coords]
+    red_midpoints = {}
+    green_midpoints = {}
 
+    for index, (case_coords, case) in enumerate(board_cases_coordinates.items()):
+        # print(case)
+        case = board_cases_coordinates[case_coords]
+        # print(case)
         # Crop the image to the coordinates of the current board case
-        case_mask = np.zeros(frame.shape[:2], dtype="uint8")
+        case_mask = np.zeros(imageFrame.shape[:2], dtype="uint8")
         case_roi = np.array(
-            [case["upLeft"], 
-            case["upRight"], 
-            case["downRight"], 
-            case["downLeft"]]
-        )
+            [case["upRight"], case["downRight"], case["downLeft"], case["upLeft"]])        
+        case_roi = [[int(point['x']), int(point['y'])] for point in case_roi]
+        case_roi = np.array(case_roi, dtype=np.int32)
+        # print(case_roi)
         cv2.fillPoly(case_mask, [case_roi], (255, 255, 255))
         case_masked = cv2.bitwise_and(hsv, hsv, mask=case_mask)
 
@@ -54,16 +58,19 @@ def get_all_pieces_coordinates(frame, board_coordinates,
         if len(red_contours) > 0:
             red_contour = red_contours[0]
             area = cv2.contourArea(red_contour)
-            if area > 100:
+            if area > 300:
                 # Calculate the centroid of the contour
                 M = cv2.moments(red_contour)
                 centroid_x = int(M['m10'] / M['m00'])
                 centroid_y = int(M['m01'] / M['m00'])
+                # print(centroid_x, centroid_y)
                 x_distance, y_distance = estimate_metric_distance(
                     frame, 
-                    board_coordinates
+                    board_coordinates, 
+                    centroid_x, 
+                    centroid_y
                 )
-                piece_centers[case_coords] = (x_distance, y_distance, "R")
+                red_midpoints[index] = (y_distance, -x_distance, "R")
 
         # Threshold the image to get the green color regions
         green_mask = cv2.inRange(case_masked, lower_green, upper_green)
@@ -83,17 +90,19 @@ def get_all_pieces_coordinates(frame, board_coordinates,
         if len(green_contours) > 0:
             green_contour = green_contours[0]
             area = cv2.contourArea(green_contour)
-            if area > 100:
+            if area > 300:
                 # Calculate the centroid of the contour
                 M = cv2.moments(green_contour)
                 centroid_x = int(M['m10'] / M['m00'])
                 centroid_y = int(M['m01'] / M['m00'])
+                print(centroid_x, centroid_y)
                 x_distance, y_distance = estimate_metric_distance(
                     frame, 
                     board_coordinates, 
                     centroid_x, 
                     centroid_y
                 )
-                piece_centers[case_coords] = (x_distance, y_distance, "G")
-
-    return piece_centers
+                green_midpoints[index] = (y_distance, -x_distance, "G")
+    print(red_midpoints)
+    print(green_midpoints)
+    return red_midpoints, green_midpoints
